c530fcabda33c4e48e3bad71aa5335df6d112432
thelyontic095 <118230658+thelyontic095@users.noreply.github.com>
2022-11-14 20:49:55 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,16 +1,12 @@
 <?php
-$hostname = $_ENV['aws-sa-east-1.connect.psdb.cloud'];
-$dbName = $_ENV['domotica'];
-$username = $_ENV['k0lb516n9itbsd1l75lc'];
-$password = $_ENV['pscale_pw_jrGZhfGpTSadJcaAcqxa9c77eQzTa9vniRUDftP41BM'];
-$ssl = $_ENV['MYSQL_ATTR_SSL_CA'];
+  $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect("aws-sa-east-1.connect.psdb.cloud","6wdvk6i9e7u0bz9o9241","pscale_pw_c7RfDvR5olnE1rAS7MKR3EC1dyvdABbM84dfxfNqUFd","domotica");
 
-$mysqli = mysqli_init();
-$mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
-$mysqli->real_connect($hostname, $username, $password, $dbName, $port);
 
 if ($mysqli->connect_error) {
     echo 'not connected to the database';
 } else {
     echo "Connected successfully";
 }
+?>
