f175389395b8ea9c4e45ddc5b9e20c086e2a1bb4
thelyontic095 <118230658+thelyontic095@users.noreply.github.com>
2022-11-14 20:51:50 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
   $mysqli = mysqli_init();
   $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-  $mysqli->real_connect("aws-sa-east-1.connect.psdb.cloud","6wdvk6i9e7u0bz9o9241","pscale_pw_c7RfDvR5olnE1rAS7MKR3EC1dyvdABbM84dfxfNqUFd","domotica");
+  $mysqli->real_connect('aws-sa-east-1.connect.psdb.cloud','6wdvk6i9e7u0bz9o9241','pscale_pw_c7RfDvR5olnE1rAS7MKR3EC1dyvdABbM84dfxfNqUFd','domotica');
 
 
 if ($mysqli->connect_error) {
